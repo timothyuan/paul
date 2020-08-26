ac8bba008224775c13dc71f0030c7be0da723ae3
Paul Heo <mheo44@gmail.com>
2020-08-26 13:26:46 -0700

    ph - added get endpoint to votes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var candidates = require('./routes/candidates.js')
+var votes = require('./routes/votes.js')
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Routes
 app.use('/candidates', candidates);
+app.use('/votes',votes);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
