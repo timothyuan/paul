145b99702e4c8091a795634153648acb2ae87ff6
Timothy Yuan <timothyuan@gmail.com>
2020-09-02 17:16:02 -0700

    add candidate dropdown menu and button for get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import axios from 'axios';
+import Select from 'react-select';
 
  class App extends React.Component{
+
    constructor(props){
      super(props);
-     this.state = { data : 'Data goes here'};
-     this.getData = this.getData.bind(this)
+     this.state = { selectedOption: null, candidateOptions: [], data : 'Data goes here'};
    }
 
-   getData(){
+   componentDidMount = () => {
+     let url = 'http://localhost:3000/candidates'
+     return axios.get(url).then(response => {
+       let candidateOptions = response.data.map(candidate => ({value: candidate.id, label: candidate.name}));
+       this.setState({candidateOptions});
+     });
+   }
+
+   handleChange = (selectedOption) => {
+     this.setState({selectedOption});
+   };
+
+   getData = () => {
      let url = 'http://localhost:3000/votes'
-     axios.get(url, { params: { candidate_id : 2, precinct_id : 832800}}).then(response => {
-       console.log(response.data)
+     axios.get(url, { params: { candidate_id : this.state.selectedOption.value, precinct_id : 832800}}).then(response => {
        this.setState({ data: response.data[0].count});
-       console.log(this.state.data)
      });
    }
 
    render(){
+     const { selectedOption, candidateOptions } = this.state;
+
      return (
        <>
-         <button onClick = {this.getData}>Get Data</button>
-         <div>{this.state.data}</div>
+        <Select
+          value = {selectedOption}
+          onChange = {this.handleChange}
+          options = {candidateOptions}
+        />
+        <button onClick = {this.getData}>Get Data</button>
+        <div>{this.state.data}</div>
        </>
      );
    }
