01042a5b2d99176f27867c0e1170d517b855c8d5
Paul Heo <mheo44@gmail.com>
2020-09-07 11:25:02 -0700

    ph - Added Sex demographic Pie chart, removed candidates select menu

--- client/src/App.js
+++ client/src/App.js
@@ -10,28 +10,31 @@ class App extends React.Component{
     super(props);
     this.state = {
       voteChart : null,
-      voteChartOptions : null
+      sexChart: null,
+      voteChartOptions : null,
+      sexChartOptions: null
+
     };
   };
 
   componentDidMount = () => {
-    axios.get('http://localhost:3000/candidates').then(response => {
-      let candidateOptions = response.data.map(candidate => ({value: candidate.id, label: candidate.name}));
-      this.setState({candidateOptions});
-    });
+    // axios.get('http://localhost:3000/candidates').then(response => {
+    //   let candidateOptions = response.data.map(candidate => ({value: candidate.id, label: candidate.name}));
+    //   this.setState({candidateOptions});
+    // });
     axios.get('http://localhost:3000/precincts').then(response => {
       let precinctOptions = response.data.map(precinct => ({value: precinct.id, label: precinct.id+', '+precinct.city+', '+precinct.county}));
       this.setState({precinctOptions});
     });
   };
 
-  handleCandidate = (selectedOption) => {
-    if(selectedOption==null){
-      this.setState({candidate_id: selectedOption});
-    }else{
-      this.setState({candidate_id: selectedOption.value});
-    }
-  };
+  // handleCandidate = (selectedOption) => {
+  //   if(selectedOption==null){
+  //     this.setState({candidate_id: selectedOption});
+  //   }else{
+  //     this.setState({candidate_id: selectedOption.value});
+  //   }
+  // };
 
   handlePrecinct = (selectedOption) => {
     if(selectedOption==null){
@@ -44,6 +47,7 @@ class App extends React.Component{
   getData = () => {
     axios.get('http://localhost:3000/votes', { params: { candidate_id : this.state.candidate_id, precinct_id : this.state.precinct_id}}).then(response => {
       this.populateVotes(response.data);
+      this.populateSex(response.data);
     });
   };
 
@@ -68,31 +72,89 @@ class App extends React.Component{
 
     // format data
     let candidates = new Map();
-    data.map(result => {
-      if (!candidates.has(result.name)){
-        candidates.set(result.name, result.count);
-      } else {
-        candidates.set(result.name, candidates.get(result.name)+result.count);
-      }
-    });
+      data.map(result => {
+        if (!candidates.has(result.name)){
+          candidates.set(result.name, result.count);
+        } else {
+          candidates.set(result.name, candidates.get(result.name)+result.count);
+        }
+      });
     chart.labels = Array.from(candidates.keys());
     chart.datasets[0].data = Array.from(candidates.values());
     this.setState({voteChart: chart});
 
     // format title
     let chartTitle = 'Votes for ';
-    if(this.state.candidate_id==null){
-      chartTitle += 'All Candidates at ';
-    }else{
-      chartTitle += this.state.candidateOptions.find(candidate => candidate.value==this.state.candidate_id).label+' at ';
-    }
     if(this.state.precinct_id==null){
       chartTitle += 'All Precincts';
     }else{
-      chartTitle += this.state.precinct_id;
+      chartTitle += 'Precinct ' + this.state.precinct_id;
     }
     this.setState({
       voteChartOptions:{
+        plugins: {
+          labels: {
+            render:'value',
+            fontColor: '#FFFFFF'
+          }
+        },
+        title: {
+          display: true,
+          text: chartTitle
+        }
+      }
+    });
+  };
+
+  populateSex = (data) => {
+    let chart = {
+      labels: [],
+      datasets: [{
+        data: [],
+        backgroundColor: [
+          '#FF6384',
+          '#FF944B',
+          '#FFCE56',
+          '#6FFF76',
+          '#73E9FE',
+          '#36A2EB',
+          '#7375FE',
+          '#A373FE',
+          '#FE73BD'
+        ]
+      }]
+    };
+
+    // format data
+    let precincts = new Map();
+    let sex = new Map([
+      ['Male',0],
+      ['Female',0],
+      ['Unknown',0]
+    ]);
+
+      data.map(result => {
+        if (!precincts.has(result.precinct_id)) {
+          sex.set('Male',sex.get('Male')+result.male)
+          sex.set('Female',sex.get('Female')+result.female)
+          sex.set('Unknown',sex.get('Unknown')+result.unknown)
+          precincts.set(result.precinct_id);
+        }
+      });
+
+    chart.labels = Array.from(sex.keys());
+    chart.datasets[0].data = Array.from(sex.values());
+    this.setState({sexChart: chart});
+
+    // format title
+    let chartTitle = 'Sex of Registered Voters in ';
+    if(this.state.precinct_id==null){
+      chartTitle += 'All Precincts';
+    }else{
+      chartTitle += 'Precinct ' + this.state.precinct_id;
+    }
+    this.setState({
+      sexChartOptions:{
         plugins: {
           labels: {
             render:'value',
@@ -121,7 +183,17 @@ class App extends React.Component{
           }
         }
       }
-    })
+    });
+    this.setState({
+      sexChartOptions:{
+        plugins:{
+          labels:{
+            render:type,
+            fontColor:'#FFFFFF'
+          }
+        }
+      }
+    });
   };
 
   render(){
@@ -131,12 +203,6 @@ class App extends React.Component{
       <>
       <Select
       isClearable = {true}
-      placeholder = {'Select Candidate'}
-      onChange = {this.handleCandidate}
-      options = {candidateOptions}
-      />
-      <Select
-      isClearable = {true}
       placeholder = {'Select Precinct'}
       onChange = {this.handlePrecinct}
       options = {precinctOptions}
@@ -144,6 +210,7 @@ class App extends React.Component{
       <button onClick = {this.getData}>Get Data</button>
       <button onClick = {this.toggle} disabled = {!this.state.voteChartOptions}>Toggle Percentage</button>
       {this.state.voteChart && <Pie data={this.state.voteChart} options={this.state.voteChartOptions}/>}
+      {this.state.sexChart && <Pie data={this.state.sexChart} options={this.state.sexChartOptions}/>}
       </>
     );
   }
