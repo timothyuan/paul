0858bda2cd688519ec52cdcf515d1692feb84835
Timothy Yuan <timothyuan@gmail.com>
2020-09-10 16:45:43 -0700

    ty - add heroku server url to react client

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component{
   };
 
   componentDidMount = () => {
-    axios.get('http://localhost:3000/precincts').then(response => {
+    axios.get('https://paul-nodeserver.herokuapp.com/precincts').then(response => {
       let precinctOptions = response.data.map(precinct => ({value: precinct.id, label: precinct.id+', '+precinct.city+', '+precinct.county}));
       this.setState({precinctOptions});
     });
@@ -41,7 +41,7 @@ class App extends React.Component{
   };
 
   getData = () => {
-    axios.get('http://localhost:3000/votes', { params: { candidate_id : this.state.candidate_id, precinct_id : this.state.precinct_id}}).then(response => {
+    axios.get('https://paul-nodeserver.herokuapp.com/votes', { params: { candidate_id : this.state.candidate_id, precinct_id : this.state.precinct_id}}).then(response => {
       this.populateDemographics(response.data);
       this.populateVotes(response.data);
     });
